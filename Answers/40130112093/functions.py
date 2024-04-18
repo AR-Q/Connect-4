c0e1ee72f9c768efc77c9aa98aeeb37626278caa
Amir Reza Qasemi <83636447+AR-Q@users.noreply.github.com>
2024-04-18 23:50:26 +0330

    Almost Finish

---
+++ Answers/40130112093/functions.py
@@ -0,0 +1,111 @@
+import numpy as np
+import copy
+
+def configure(x):
+    return (x+1)*100
+
+def configure_pos(x,y):
+
+    # x = 4 - x
+    # y = 4 - y
+
+    return (configure(x), configure(y))
+
+def configure_row(row):
+
+    configured_row = copy.deepcopy(row)
+
+    for i in range(5):
+
+        if row[i] == 4:
+            configured_row[i] = 500
+        elif row[i] == 3:
+            configured_row[i] = 400
+        elif row[i] == 2:
+            configured_row[i] = 300
+        elif row[i] == 1:
+            configured_row[i] = 200
+        elif row[i] == 0:
+            configured_row[i] = 100
+    
+    return configured_row
+
+def get_bottom_row(board: np.array):
+
+    row = [-1,-1,-1,-1,-1]
+
+    for i in range(4, -1, -1):
+        for j in range(5):
+
+            if board[i][j] == 0 and row[j] == -1:
+                row[j] = i
+
+    return row
+
+
+
+def endgame(board: np.array, turn):
+    if v_win(board, turn):
+        return turn
+    elif h_win(board, turn):
+        return turn
+    elif draw(board):
+        return 0
+    
+    return -1
+
+        
+def v_win(board: np.array, turn):
+    
+    for i in range(5):
+        counter = 0
+        for j in range(5):
+            if board[j][i] == turn:
+                counter += 1
+            elif i != 4:
+                counter = 0
+        
+        if counter >= 4:
+            return True
+    
+    return False
+
+def h_win(board: np.array, turn):
+
+    for i in range(5):
+        counter = 0
+        for j in range(5):
+            if board[i][j] == turn:
+                counter += 1
+            elif j != 4:
+                counter = 0
+        
+        if counter >= 4:
+            return True
+    
+    return False
+
+# def d_win(board: np.array, turn):
+    
+#     for i in range(2):
+#         counter = 0
+#         for j in range(5):
+
+
+def draw(board: np.array):
+    for i in range(5):
+        for j in range(5):
+            if board[i][j] != 0:
+                return False
+            
+    return True
+
+a = np.array([
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [1,0,0,0,0]
+])
+
+# print(get_bottom_row(a))
--- Answers/40130112093/main.py
+++ Answers/40130112093/main.py
@@ -1,19 +1,26 @@
 import pygame
 import numpy as np
+from functions import *
 
 pygame.init()
-screan = pygame.display.set_mode((600,600))
+screan = pygame.display.set_mode((850,600))
 clock = pygame.time.Clock()
 running = True
 turn = 0
 font = pygame.font.Font('freesansbold.ttf', 18)
 
-game = True
+text_player1 = font.render('Player 1 is Red', True,"red","cyan")
+textRect_player1 = text_player1.get_rect()
+textRect_player1.center = (700,100)
+text_player2 = font.render('Player 2 is Blue', True,"blue","cyan")
+textRect_player2 = text_player2.get_rect()
+textRect_player2.center = (700,150)
 
-board = np.zeros((3,3))
+game = -1
 
+board = np.zeros((5,5))
 
-
+X = 0
 
 while running:
     
@@ -21,18 +28,107 @@ while running:
 
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONUP and game != -1:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONUP and game == -1:
+            if select_row[X] >= 0:
+                board[select_row[X]][X] = turn + 1
+                game = endgame(board,turn +1)
+                turn = (turn + 1) % 2
+                print(board)
+            
+
         
             
 
 
     screan.fill("cyan")
 
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(50,50,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(150,50,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(250,50,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(350,50,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(450,50,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(50,150,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(150,150,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(250,150,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(350,150,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(450,150,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(50,250,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(150,250,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(250,250,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(350,250,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(450,250,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(50,350,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(150,350,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(250,350,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(350,350,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(450,350,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(50,450,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(150,450,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(250,450,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(350,450,100,100),1)
+    pygame.draw.rect(screan, (0,0,0), pygame.Rect(450,450,100,100),1)
+    
+    
+    screan.blit(text_player1, textRect_player1)
+    screan.blit(text_player2, textRect_player2)
+    text_turn = font.render(f'Turn : player {turn + 1}', True,"black","cyan")
+    textRect_turn = text_turn.get_rect()
+    textRect_turn.center = (700,200)
+    screan.blit(text_turn, textRect_turn)
+
+
+    for i in range(5):
+        for j in range(5):
+            if board[i][j] == 1:
+                pygame.draw.circle(screan, "red", configure_pos(j,i), 30)
+            elif board[i][j] == 2:
+                pygame.draw.circle(screan, "blue", configure_pos(j,i), 30)
+
+
+
+
+    if game == -1:
+        (x,y) = pygame.mouse.get_pos()
 
     
+        if turn == 0:
+            color = (255,133,133)
+        else:
+            color = (163, 209, 255)
+
+        select_row = get_bottom_row(board)
+        configure_selected_row = configure_row(select_row)
+
+        if 50 < x < 150:
+            pygame.draw.circle(screan, color, (100,configure_selected_row[0]), 30)
+            X = 0
+        elif 150 < x < 250:
+            pygame.draw.circle(screan, color, (200,configure_selected_row[1]), 30)
+            X = 1
+        elif 250 < x < 350:
+            pygame.draw.circle(screan, color, (300,configure_selected_row[2]), 30)
+            X = 2
+        elif 350 < x < 450:
+            pygame.draw.circle(screan, color, (400,configure_selected_row[3]), 30)
+            X = 3
+        elif 450 < x < 550:
+            pygame.draw.circle(screan, color, (500,configure_selected_row[4]), 30)
+            X = 4
+    else:
+        text_winner = font.render(f'The WINNER is Player{game}', True,"yellow","cyan")
+        textRect_winner = text_winner.get_rect()
+        textRect_winner.center = (700,250)
+        screan.blit(text_turn, textRect_turn)
     
     pygame.display.flip()
 
     clock.tick(60)
+
+
+
+
 
 pygame.quit()
 exit()
